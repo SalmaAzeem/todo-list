3539038610422b6de7db8a7368ed8f4fd71ae61d
SalmaAzeem <s-salma.azeem@zewailcity.edu.eg>
2025-06-29 18:20:11 +0300

    feat: add list functionalities (add, delete, toggle)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todo-list';
+  tasks: Task[] = [
+    new Task("visit the doctor"), 
+    new Task("buy groceries"), 
+    new Task("clean the house")
+  ];
+  add(newtask: string) {
+    console.log(newtask);
+    this.tasks.push(new Task(newtask));
+  }
+  remove(task: Task) {
+    let confirmRemove = confirm(`Are you sure you want to remove "${task.title}"?`);
+    if (confirmRemove) {
+      this.tasks = this.tasks.filter(t => t !== task);
+    }
+  }
+}
+
+class Task {
+  constructor(public title: string) {}
+  toggleIsDone() {
+    this.is_done = !this.is_done;
+  }
+  public is_done: boolean = false;
 }
